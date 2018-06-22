3d38e97467d9d34a3df3bf10b45924b3e46280bd
Smith <nsmith@aecio.com>
2018-06-22 10:11:21 -0400

    Added pickers for focused month and year to filter screen

--- src/screens/Filter/Filter.js
+++ src/screens/Filter/Filter.js
@@ -1,16 +1,65 @@
 import React, { Component } from 'react'; 
-import { View, Text, StyleSheet } from 'react-native'; 
+import { View, Text, StyleSheet, Picker } from 'react-native'; 
+import { connect } from 'react-redux';
 
 class FilterScreen extends Component {
 
+    handleMonthChange = (month, index) => {
+        alert('Month selected: ' + month); 
+    }
+    
+    handleYearChange = (year, index) => {
+        alert('Year selected: ' + year); 
+    }
 
     render() {
+        const { focusedDates } = this.props; 
+        const today = new Date(); 
+
         return (
             <View>
-                <Text>Filter Modal</Text>
+                <Text>Select Filter</Text>
+                <View>
+                    <View>
+                        <Text>Month</Text>
+                        <Picker 
+                            selectedValue={focusedDates && focusedDates.month}
+                            onValueChange={this.handleMonthChange}
+                        >
+                            <Picker.Item label="January" value={1} />
+                            <Picker.Item label="February" value={2} />
+                            <Picker.Item label="March" value={3} />
+                            <Picker.Item label="April" value={4} />
+                            <Picker.Item label="May" value={5} />
+                            <Picker.Item label="June" value={6} />
+                            <Picker.Item label="July" value={7} />
+                            <Picker.Item label="August" value={8} />
+                            <Picker.Item label="September" value={9} />
+                            <Picker.Item label="October" value={10} />
+                            <Picker.Item label="November" value={11} />
+                            <Picker.Item label="December" value={12} />
+                        </Picker>
+                    </View>
+                    <View>
+                        <Text>Year</Text>
+                        <Picker 
+                            selectedValue={focusedDates && focusedDates.year}
+                            onValueChange={this.handleYearChange}
+                        >
+                            <Picker.Item label={today.getFullYear() + ''} value={today.getFullYear()} />
+                            <Picker.Item label={(today.getFullYear() - 1) + ''} value={today.getFullYear() - 1} />
+                        </Picker>
+                    </View>
+                </View>
             </View>
         ); 
     }
 }
 
-export default FilterScreen; 
+const mapStateToProps = state => {
+    return {
+        focusedDates: state.transactions.trackedDates
+    }
+}
+
+export default connect(mapStateToProps)(FilterScreen); 
